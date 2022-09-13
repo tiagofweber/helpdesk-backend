625524696605b6176d36db92e494097be75daf26
Tiago Weber <webertiagof@gmail.com>
2022-09-12 21:36:06 -0300

    Adiciona autorização com JWT

--- src/main/java/com/tweber/helpdesk/controllers/TecnicoController.java
+++ src/main/java/com/tweber/helpdesk/controllers/TecnicoController.java
@@ -5,6 +5,7 @@ import com.tweber.helpdesk.domain.dtos.TecnicoDTO;
 import com.tweber.helpdesk.services.TecnicoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -33,6 +34,7 @@ public class TecnicoController {
         return ResponseEntity.ok(tecnicosDTO);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PostMapping
     public ResponseEntity<TecnicoDTO> create(@Valid @RequestBody TecnicoDTO tecnicoDTO) {
         Tecnico tecnico = service.create(tecnicoDTO);
@@ -40,12 +42,14 @@ public class TecnicoController {
         return ResponseEntity.created(uri).build();
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PutMapping(value = "/{id}")
     public ResponseEntity<TecnicoDTO> update(@PathVariable Long id, @Valid @RequestBody TecnicoDTO tecnicoDTO) {
         Tecnico tecnico = service.update(id, tecnicoDTO);
         return ResponseEntity.ok(new TecnicoDTO(tecnico));
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<TecnicoDTO> delete(@PathVariable Long id) {
         service.delete(id);
